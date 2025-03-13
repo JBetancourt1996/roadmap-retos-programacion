d948260a7bbaa59994eaaed061bb26545c3b4d78
jorbe <you@example.com>
2025-03-12 20:25:06 -0600

    ejercicio js  00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JBetancourt1996.js
@@ -0,0 +1,44 @@
+
+//Documentacion oficial  de JavaScript
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Dos diagonales puede escribir un texto
+
+/*Con esta sintaxis diagonal/ * * /diagonal 
+puedes escribir varias lineas
+de texto
+*/
+
+const name = 'JBetancourt1996';
+console.log(name);
+
+//Numero (Number)
+const edad = 25;
+//numero decimales
+const precio = 9.99
+
+// Cadena de texto (String)
+const nombre = "Jorge";
+const saludo = 'Hola, Como estas';
+
+//Booleano (Boolean)
+const esMayorDeEdad = true;
+const tienePermiso = false;
+
+//Nulo (Null)
+const valorNulo = null;
+
+//Indifinido (Undefined)
+// const valorIndefinido;
+
+//Símbolo (Symbol):
+const id = Symbol('id');
+/* Estos son los tipos de datos primitivos en JavaScript.
+ Cada uno tiene su propia utilidad y se utilizan en diferentes contextos según las necesidades del programa. 
+ ¡Espero que esto te sea útil! */
+
+ const saludo2 = 'Hola, JAVASCRIPT';
+ console.log(saludo2)
+
+
+
